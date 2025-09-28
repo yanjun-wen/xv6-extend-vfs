346eb4d44dc2b818b54f9631dab7381741a5ca74
Yanjun Wen <38742170+yanjun-wen@users.noreply.github.com>
2025-09-28 19:40:14 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void main() {
+  int inf = open("/test1", O_RDONLY, 0);
+  int outf = open("/mnt/test1", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+  int i;
+  char buf[4096];
+  do {
+    i = read(inf, buf, 4096);
+    write(outf, buf, i);
+  } while (i);
+  close(outf);
+  close(inf);
+}
