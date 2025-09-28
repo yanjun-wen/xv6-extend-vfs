9031659a70aa377e1c3e9ebfe4ee34547e9fc856
Yanjun Wen <38742170+yanjun-wen@users.noreply.github.com>
2025-09-28 20:43:40 +0800

    Add files via upload

--- test1.c
+++ test1.c
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 
 void main() {
-  int inf = open("/test1", O_RDONLY, 0);
+  int inf = open("/tests/test1", O_RDONLY, 0);
   int outf = open("/mnt/test1", O_WRONLY | O_CREAT | O_TRUNC, 0600);
   int i;
   char buf[4096];
